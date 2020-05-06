2033ea15b2f9749a5f97a2010ae38bfe9d448f5d
Balaji Rajendran <40071466+bala3ji@users.noreply.github.com>
2020-05-06 20:43:37 +0530

    Updated service to show temperature in Celsius

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,7 +9,7 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
   getWeather(location){
     return this.http.get(
-      'https://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=09133156e084391949d67afe2ae9eb63'
+      'https://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=09133156e084391949d67afe2ae9eb63&units=metric'
     );
 }
 
