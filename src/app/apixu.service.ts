dd264b5832d4ef1f10b48c250381dcf72d7f2f4e
Balaji Rajendran <40071466+bala3ji@users.noreply.github.com>
2020-05-06 18:27:04 +0530

    https to http for replicating issue

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,7 +9,7 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
   getWeather(location){
     return this.http.get(
-        'https://api.weatherstack.com/current?access_key=ab48a76276b31522b2f82c9ef22ccc90&query=' + location
+        'http://api.weatherstack.com/current?access_key=ab48a76276b31522b2f82c9ef22ccc90&query=' + location
     );
 }
 
